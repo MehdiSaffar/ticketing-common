93720beea0fc965fcc729ec2bf00f706ebddb381
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-08-26 18:44:01 +0300

    updates

--- src/events/ticket-created-event.ts
+++ src/events/ticket-created-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from './subjects'
 
-export default interface TicketCreatedEvent {
+export interface TicketCreatedEvent {
     subject: Subjects.TicketCreated
     data: {
         id: string
--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from './subjects'
 
-export default interface TicketUpdatedEvent {
+export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated
     data: {
         id: string
