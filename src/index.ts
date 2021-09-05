533528e086b1cf3556d0fbef9a86c2589f4ace70
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-09-05 15:44:48 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,5 @@ export * from './events/order-cancelled-event'
 export * from './events/subjects'
 
 export * from './events/types/order-status'
+
+export * from './events/expiration-complete-event'
